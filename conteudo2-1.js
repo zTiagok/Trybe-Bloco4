d5022a94507d9dc700d32fd0d59eefeb4232e01c
Tiago Braga Costa <ztiagok@gmail.com>
2022-04-07 15:28:20 -0300

    Resolução das atividades do segundo conteúdo

---
+++ conteudo2-1.js
@@ -0,0 +1,9 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(index_groceryList = 0; index_groceryList <= groceryList.length; index_groceryList+=1 )
+{
+    
+    console.log(groceryList[index_groceryList - 1]);
+}
+
+// FEITO!
