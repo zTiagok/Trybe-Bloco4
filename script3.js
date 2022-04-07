7c410ccdacbf13b8625324da9a5680c1cd688ab8
Tiago Braga Costa <ztiagok@gmail.com>
2022-04-07 18:28:34 -0300

    Script3 Feito

---
+++ script3.js
@@ -0,0 +1,16 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for (let index of numbers)
+{
+
+    sum += index;
+}
+
+let division = sum / numbers.length;
+
+console.log(division);
+
+// FEITO!
+
+
