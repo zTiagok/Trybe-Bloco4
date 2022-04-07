31a1ab1cbc4b750388a275cd5cbdea6ed7332083
Tiago Braga Costa <ztiagok@gmail.com>
2022-04-07 18:14:08 -0300

    Script2 Feito

---
+++ script2.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for (index = 0; index < numbers.length; index += 1)
+{
+
+    sum += numbers[index]
+}
+
+console.log(sum)
+
+// FEITO!
