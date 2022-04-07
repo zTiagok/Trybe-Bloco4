c1bac4e5ce7ccce0518ddb7098b1beec255ab50b
Tiago Braga Costa <ztiagok@gmail.com>
2022-04-07 18:29:29 -0300

    Script3 Feito]

--- script3.js
+++ script3.js
@@ -1,16 +1,14 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; // DECLARA AS ARRAYS 
+let sum = 0; // DECLARA A VAR DE SOMA 
 
-for (let index of numbers)
+for (let index of numbers) // ESTRUTURA DE REPETIÇÃO FOR/OF, MESMO CONCEITO DA REPETIÇÃO FOR
 {
 
-    sum += index;
+    sum += index; // A CADA LOOP, 'SUM' RECEBE O VALOR DA ARRAY PASSADA, SOMANDO COM A PRÓXIMA, ASSIM FAZENDO A SOMA EM SEQUÊNCIA
 }
 
-let division = sum / numbers.length;
+let division = sum / numbers.length; // TOTAL DA SOMA DE TODAS AS ARRAYS EM 'SUM' DIVIDO PELA QUANTIDADE TOTAL DE ARRAYS CONTIDA EM 'NUMBERS'
 
 console.log(division);
 
 // FEITO!
-
-
